eaf380a9da999ba36e15a911e6d37050d285ba36
mmslotin <balu54@yandex.ru>
2022-09-13 16:16:49 +0300

    Init commit from console

---
+++ src/test/java/guru/qa/ExampleTest.java
@@ -0,0 +1,17 @@
+package guru.qa;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class ExampleTest {
+
+    @Test
+    void exampleTest0() {
+        Assertions.assertTrue (33 > 11);
+    }
+
+    @Test
+    void exampleTest1() {
+        Assertions.assertTrue (11 > 9);
+    }
+}
